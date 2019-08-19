1d559a9f9f9827dba83eaaf0557f6461e3cb0596
r3alfak3 <stefanisaia@gmail.com>
2019-08-19 03:15:00 +0000

    Added TDD functionality for the editing, updating & destroying records with logged in users

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
 	end
@@ -19,12 +19,43 @@ class GramsController < ApplicationController
 
 	def show
 		@gram = Gram.find_by_id(params[:id])
-		if @gram.blank?
-			render plain: 'Not Found :(', status: :not_found
-		end
+    return render_not_found if @gram.blank?
+	end
+
+	def edit
+		@gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    if @gram.user != current_user
+    	render plain: 'Forbidden :(' , status: :forbidden
+    end
+	end
+
+	def update
+	  @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+	  return render_not_found(:forbidden) if @gram.user != current_user
+	  @gram.update_attributes(gram_params)
+	  if @gram.valid?
+	    redirect_to root_path
+	  else
+	    return render :edit, status: :unprocessable_entity
+	  end
+	end
+
+	def destroy
+		@gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+		return render_not_found(:forbidden) if @gram.user != current_user
+		@gram.destroy
+		redirect_to root_path
 	end
 
 	private
+
+	def render_not_found(status=:not_found)
+		render plain: '#{status.to_s.titleize} :(', status: status
+	end
 
 	def gram_params
 	  params.require(:gram).permit(:message)
